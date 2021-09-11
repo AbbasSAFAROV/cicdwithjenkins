c321051c936dde432a0cfd6eadae8510d7e7b5e7
FIRST_NAME LAST_NAME <abbasabdullaziz@gmail.com>
2021-09-11 04:00:25 +0300

    first commit

---
+++ src/main/java/com/CIWithJenkins/base/HomeController/HomeController.java
@@ -0,0 +1,17 @@
+package com.CIWithJenkins.base.HomeController;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HomeController {
+
+    @GetMapping("/")
+    public String hello(){
+        return "hello from jenkins!!";
+    }
+
+
+}
